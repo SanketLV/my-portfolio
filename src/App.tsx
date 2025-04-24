b7b523711d738d48240a9fe4053529097a622b55
SanketLV <sanketlakhani80@gmail.com>
2025-04-24 12:52:33 +0530

    project setup, navbar added and Hero section partially completed

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonials */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
